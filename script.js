ccded3a0e3ec12653b32bdb8884a61b1a374e694
Sharear Ahammed Nihal <106548719+sharearahammed@users.noreply.github.com>
2023-08-03 22:30:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        quistion: 'How old is Florin?',
+        a:'10',
+        b:'17',
+        c:'26',
+        d:'110',
+        correct:'c'
+    },
+    {
+        quistion: 'What is the best used programing language in 2019?',
+        a:'Java',
+        b:'C',
+        c:'C++',
+        d:'Python',
+        correct:'a'
+    },
+    {
+        quistion: 'Who is the president of US?',
+        a:'Florin Pop',
+        b:'Donald Trump',
+        c:'Ivan Saldano',
+        d:'Mihal Andrei',
+        correct:'b'
+    },
+    {
+        quistion: 'What oes HTML stand for?',
+        a:'Hypertext Markup Language',
+        b:'Cascad Style Sheet',
+        c:'Jason Object Notation',
+        d:'Helicopters Terminal',
+        correct:'a'
+    },
+    {
+        quistion: 'What year was JavaScript launched?',
+        a:'1996',
+        b:'1995',
+        c:'2018',
+        d:'none of the avobe',
+        correct:'d'
+    },
+]
